48c1f0add998c0f57653e88f2f11dedbe54c64d5
Chris Risden <christopherrisden@gmail.com>
2022-02-04 21:06:45 -0800

    not yet placing ships, but able to attack!

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import {createShip, createGameboard } from './gameLogic.js'
 
 // on website load, what do we want this thing to do! we want.... to create our boards (both front/back end) and
 //link them up to each other :)
+const cpu = createGameboard()
 function startGame () {
-  const cpu = createGameboard()
   const human = createGameboard()
   cpu.randomlyPlace();
   let humanTurn = true;
@@ -18,9 +18,22 @@ function createBothGrids () {
     let cpuCell = document.createElement('div');
     humanCell.id = `human-${i}`
     cpuCell.id = `cpu-${i}`
+    cpuCell.addEventListener('click', clickComputerBoard)
     human.appendChild(humanCell)
     cpu.appendChild(cpuCell)
+
   }
 }
+
+function clickComputerBoard (e) {
+  const id = this.id.slice(4)
+  console.log(this, cpu.gameboard[id])
+  if (cpu.gameboard[id] === 1) {
+    this.classList.add('ship')
+  } else {
+    this.classList.add('not-ship')
+  }
+}
+
 startGame();
 createBothGrids();
